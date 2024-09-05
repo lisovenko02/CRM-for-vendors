f3742868271dbea6d6b0d4645881ed529b859708
lisovenko02 <vanyalisovenkoo@gmail.com>
2024-09-06 00:15:16 +0300

    finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Sidebar from './components/sidebar';
+
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
+      <Sidebar />
     </main>
   );
 }
